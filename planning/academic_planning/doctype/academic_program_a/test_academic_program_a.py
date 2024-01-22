9f052d0cf43ad3d52a15d89466b7e95002ae2548
Mohammed Altheeb <altheebinfo@gmail.com>
2024-01-23 02:10:58 +0300

    feat(core):Initialize Core Doctype with Fields and Functionalities

---
+++ planning/academic_planning/doctype/academic_program_a/test_academic_program_a.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, mohammed altheeb and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAcademicPrograma(FrappeTestCase):
+	pass
